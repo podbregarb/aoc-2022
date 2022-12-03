1148d7dc798f8d6f3c647ce1c2f004328d4de785
Barbka Podbregar <barbka.podbregar@outfit7.com>
2022-12-03 06:47:11 +0100

    Third day + 4 template

--- src/main/3/main.go
+++ src/main/3/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strings"
 )
 
 //go:embed testInput.txt
@@ -10,6 +11,8 @@ var testInput string
 
 //go:embed input.txt
 var input string
+
+var alphabet string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func main() {
 	fmt.Println("Test input")
@@ -25,6 +28,32 @@ func printAnswers(s string) {
 }
 
 func getAnswers(s string) (int, int) {
+	split := strings.Split(s, "\n")
+	priority := 0
+	for i := range split {
+		firstHalf := split[i][0 : len(split[i])/2]
+		secondHalf := split[i][len(split[i])/2 : len(split[i])]
+		for i2 := range firstHalf {
+			u := string(firstHalf[i2])
+			if strings.Contains(secondHalf, u) {
+				priority += strings.Index(alphabet, u) + 1
+				break
+			}
+		}
+	}
 
-	return 0, 0
+	priorityBadges := 0
+	for i := 0; i < len(split)-1; i += 3 {
+		firstLine := split[i]
+		secondLine := split[i+1]
+		thirdLine := split[i+2]
+		for i2 := range firstLine {
+			u := string(firstLine[i2])
+			if strings.Contains(secondLine, u) && strings.Contains(thirdLine, u) {
+				priorityBadges += strings.Index(alphabet, u) + 1
+				break
+			}
+		}
+	}
+	return priority, priorityBadges
 }
