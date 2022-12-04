d731e4c1925286edbd2650986b9be38cea4aa638
Barbka Podbregar <barbka.podbregar@outfit7.com>
2022-12-04 08:39:45 +0100

    Day 4

--- src/main/4/main.go
+++ src/main/4/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 //go:embed testInput.txt
@@ -25,6 +27,23 @@ func printAnswers(s string) {
 }
 
 func getAnswers(s string) (int, int) {
-
-	return 0, 0
+	split := strings.Split(s, "\n")
+	containedRanges := 0
+	overlappedRanges := 0
+	for i := 0; i < len(split)-1; i++ {
+		splitRow := strings.Split(split[i], ",")
+		range1 := strings.Split(splitRow[0], "-")
+		range2 := strings.Split(splitRow[1], "-")
+		range11, _ := strconv.Atoi(range1[0])
+		range12, _ := strconv.Atoi(range1[1])
+		range21, _ := strconv.Atoi(range2[0])
+		range22, _ := strconv.Atoi(range2[1])
+		if range11 >= range21 && range12 <= range22 || (range21 >= range11 && range22 <= range12) {
+			containedRanges++
+		}
+		if range11 <= range22 && range21 <= range12 {
+			overlappedRanges++
+		}
+	}
+	return containedRanges, overlappedRanges
 }
