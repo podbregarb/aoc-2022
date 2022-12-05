7bdc465dbc5011cde2fe270a365679045c5d60f8
Barbka Podbregar <barbka.podbregar@outfit7.com>
2022-12-05 09:14:10 +0100

    Day 5

--- src/main/5/main.go
+++ src/main/5/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 //go:embed testInput.txt
@@ -20,11 +22,62 @@ func main() {
 
 func printAnswers(s string) {
 	first, second := getAnswers(s)
-	fmt.Printf("First answer: %d\n", first)
-	fmt.Printf("Second answer: %d\n", second)
+	fmt.Printf("First answer: %s\n", first)
+	fmt.Printf("Second answer: %s\n", second)
 }
 
-func getAnswers(s string) (int, int) {
+func getAnswers(s string) (string, string) {
+	return getTopCrates(s, false), getTopCrates(s, true)
+}
 
-	return 0, 0
+func getTopCrates(s string, retainOrder bool) string {
+	split := strings.Split(s, "\n")
+	instructionsStart := getStartOfInstructions(split)
+	cratesDefinition := split[:instructionsStart]
+	cratesIndexes := strings.Fields(split[instructionsStart-1])
+	numOfCrates := len(cratesIndexes)
+	crates := make([][]string, numOfCrates)
+	for crateRowIndex := len(cratesDefinition) - 2; crateRowIndex >= 0; crateRowIndex-- {
+		row := cratesDefinition[crateRowIndex]
+		for crateNumberIndex := range cratesIndexes {
+			indexOfCrate := strings.Index(split[instructionsStart-1], cratesIndexes[crateNumberIndex])
+			if len(row) > indexOfCrate && (string(row[indexOfCrate]) != " ") {
+				crates[crateNumberIndex] = append(crates[crateNumberIndex], string(row[indexOfCrate]))
+			}
+		}
+	}
+	instructions := split[instructionsStart+1:]
+	for instructionIndex := 0; instructionIndex < len(instructions)-1; instructionIndex++ {
+		fields := strings.Fields(instructions[instructionIndex])
+		howMany, _ := strconv.Atoi(fields[1])
+		from, _ := strconv.Atoi(fields[3])
+		to, _ := strconv.Atoi(fields[5])
+		if retainOrder {
+			crates[to-1] = append(crates[to-1], crates[from-1][len(crates[from-1])-howMany:]...)
+		} else {
+			crates[to-1] = append(crates[to-1], reverse(crates[from-1][len(crates[from-1])-howMany:])...)
+		}
+		crates[from-1] = crates[from-1][:len(crates[from-1])-howMany]
+	}
+	topCrates := ""
+	for crate := range crates {
+		topCrates += crates[crate][len(crates[crate])-1]
+	}
+	return topCrates
+}
+
+func getStartOfInstructions(s []string) int {
+	for i := range s {
+		if s[i] == "" {
+			return i
+		}
+	}
+	return -1
+}
+
+func reverse[S ~[]E, E any](s S) S {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+	return s
 }
