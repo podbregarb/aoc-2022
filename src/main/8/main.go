32f7db1e6e5e740f6040f718fd048fe23853f820
Barbka Podbregar <barbka.podbregar@outfit7.com>
2022-12-08 11:03:03 +0100

    Day 8

--- src/main/8/main.go
+++ src/main/8/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strings"
 )
 
 //go:embed testInput.txt
@@ -25,6 +26,59 @@ func printAnswers(s string) {
 }
 
 func getAnswers(s string) (int, int) {
+	split := strings.Split(s, "\n")
+	visibleTrees := 0
+	scenicScore := 0
+	for i := 0; i < len(split); i++ {
+		for j := 0; j < len(split[i]); j++ {
+			visibleFromLeft, scenicScoreLeft := isVisibleFromLeft(split, i, j)
+			visibleFromRight, scenicScoreRight := isVisibleFromRight(split, i, j)
+			visibleFromTop, scenicScoreTop := isVisibleFromTop(split, i, j)
+			visibleFromBelow, scenicScoreBelow := isVisibleFromBelow(split, i, j)
+			if visibleFromLeft || visibleFromRight || visibleFromTop || visibleFromBelow {
+				visibleTrees++
+			}
+			newScenicScore := scenicScoreLeft * scenicScoreRight * scenicScoreTop * scenicScoreBelow
+			if newScenicScore > scenicScore {
+				scenicScore = newScenicScore
+			}
+		}
+	}
+	return visibleTrees, scenicScore
+}
 
-	return 0, 0
+func isVisibleFromTop(split []string, i int, j int) (bool, int) {
+	for index := i; index > 0; index-- {
+		if split[index-1][j] >= split[i][j] {
+			return false, i - index + 1
+		}
+	}
+	return true, i
+}
+
+func isVisibleFromBelow(split []string, i int, j int) (bool, int) {
+	for index := i; index < len(split)-1; index++ {
+		if split[index+1][j] >= split[i][j] {
+			return false, index - i + 1
+		}
+	}
+	return true, len(split) - i - 1
+}
+
+func isVisibleFromLeft(split []string, i int, j int) (bool, int) {
+	for index := j; index > 0; index-- {
+		if split[i][index-1] >= split[i][j] {
+			return false, j - index + 1
+		}
+	}
+	return true, j
+}
+
+func isVisibleFromRight(split []string, i int, j int) (bool, int) {
+	for index := j; index < len(split[i])-1; index++ {
+		if split[i][index+1] >= split[i][j] {
+			return false, index - j + 1
+		}
+	}
+	return true, len(split[i]) - j - 1
 }
